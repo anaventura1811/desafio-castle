440402eeabd6110cb961197bf39a1750b8186764
Ana Ventura <anaventura1811@gmail.com>
2021-08-02 12:26:04 -0300

    feat: adiciona estado à pág. Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductsList from '../components/ProductsList';
 
 function Home() {
+  const [products, setProducts] = useState([]);
+  
   return (
     <div>
       <ProductsList />
