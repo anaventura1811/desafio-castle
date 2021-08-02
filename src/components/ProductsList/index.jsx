74a9f2e2f5eae5f6729905a215fdadea8e990e17
Ana Ventura <anaventura1811@gmail.com>
2021-08-02 16:19:43 -0300

    fix: remove importações desnecessárias de ProductsList

--- src/components/ProductsList/index.jsx
+++ src/components/ProductsList/index.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import camiseta from '../../images/camiseta-frente-09.png';
-import { IoMdHeartEmpty } from 'react-icons/io';
-import tagIcon from '../../images/tagIcon.svg';
 import './styles.scss';
 import Card from '../Card';
 
@@ -16,7 +14,7 @@ function ProductsList({ products }) {
 
   return (
 		<ul className='products-list'>
-      <Card product={ productTest} />
+      <Card product={productTest} />
 
 			{products.map((product) => (
 				<Card key={product.id} product={product} />
