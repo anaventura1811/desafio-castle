6a24cbfcec9e97a2f157740fb64ea08ca6cf929c
Ana Ventura <anaventura1811@gmail.com>
2021-08-02 12:02:35 -0300

    feat: pasta components, cria componente ProductsList

---
+++ src/components/ProductsList/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function ProductsList({ products }) {
+  return (
+    <ul>
+      { products.map((product) => (
+        <li key={ product.id}>
+          <img src={product.image} alt={ product.title }/>
+          <strong>{ product.title }</strong>
+          <span>{ product.priceFormatted }</span>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default ProductsList;
